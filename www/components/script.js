5085a7048d824df8663ecc2f4fe962c5a246fa1e
KaykSantos <kaykarrudasantos2018@gmail.com>
2022-06-21 00:12:11 +0900

    Atualização: botões diferentes para cotação de cada moeda

--- www/components/script.js
+++ www/components/script.js
@@ -1,6 +1,7 @@
 window.onload = ()=>{
-    const cotacao = document.querySelector('#cotacao');
-    cotacao.addEventListener('click', ()=>{
+    const usdbrl = document.querySelector('#usdbrl');
+    const btcbrl = document.querySelector('#btcbrl');
+    usdbrl.addEventListener('click', ()=>{
         const opcoes = {
             rethod: 'GET',
             mode: 'cors',
@@ -15,6 +16,25 @@ window.onload = ()=>{
                     document.querySelector('#variacao').textContent = data.USDBRL.varBid;
                     document.querySelector('#maxima').textContent = data.USDBRL.high;
                     document.querySelector('#minima').textContent = data.USDBRL.low;
+                })
+            }
+        )
+    })
+    btcbrl.addEventListener('click', ()=>{
+        const opcoes = {
+            rethod: 'GET',
+            mode: 'cors',
+            cache: 'default'
+        }
+        fetch(`https://economia.awesomeapi.com.br/json/last/BTC-BRL`, opcoes)
+        .then(
+            response => { response.json()
+                .then(data => {
+                    document.querySelector('#compra').textContent = data.BTCBRL.bid;
+                    document.querySelector('#venda').textContent = data.BTCBRL.ask;
+                    document.querySelector('#variacao').textContent = data.BTCBRL.varBid;
+                    document.querySelector('#maxima').textContent = data.BTCBRL.high;
+                    document.querySelector('#minima').textContent = data.BTCBRL.low;
                 })
             }
         )
